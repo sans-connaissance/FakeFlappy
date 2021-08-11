de245253c83f23060b8931a5652321195b03e663
sans-connaissance <davemalicke@gmail.com>
2021-08-10 21:50:24 -0400

    Added anti-gravity touch control

--- FakeFlappy/GameScene.swift
+++ FakeFlappy/GameScene.swift
@@ -11,6 +11,7 @@ import GameplayKit
 class GameScene: SKScene {
     
     let player = SKSpriteNode(imageNamed: "plane")
+    var touchingScreen = false
     
     override func didMove(to view: SKView) {
         //add gravity to the GameScene node
@@ -21,40 +22,47 @@ class GameScene: SKScene {
         
         player.position = CGPoint(x: -400, y: 250)
         addChild(player)
-
+        
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        
+        touchingScreen = true
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
- 
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        touchingScreen = false
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-   
+        touchingScreen = false
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        if touchingScreen {
+            player.physicsBody?.velocity = CGVector(dx: 0, dy: 300)
+        }
+        
     }
 }
